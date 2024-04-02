40550f83b3b68ad52ea48adb74d5c9c46e214b18
shreyamishra414@gmail.com <shreyamishra414@gmail.com>
2024-04-02 21:44:30 +0530

    INIT Spring Boot Repository

---
+++ ooad/src/main/java/com/bookonrails/ooad/OoadApplication.java
@@ -0,0 +1,13 @@
+package com.bookonrails.ooad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OoadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OoadApplication.class, args);
+	}
+
+}
---
+++ ooad/src/test/java/com/bookonrails/ooad/OoadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookonrails.ooad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OoadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
