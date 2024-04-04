bc46b3e454032601aa1cb6f2bda80c1a7a703311
shreyamishra414@gmail.com <shreyamishra414@gmail.com>
2024-04-04 19:06:34 +0530

    add admin and feedback model

--- ooad/src/main/java/com/bookonrails/ooad/Model/Admin.java
+++ ooad/src/main/java/com/bookonrails/ooad/Model/Admin.java
@@ -1,5 +1,37 @@
 package com.bookonrails.ooad.Model;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Admin {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    private String username;
+    private String password;
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 }
--- ooad/src/main/java/com/bookonrails/ooad/Model/Feedback.java
+++ ooad/src/main/java/com/bookonrails/ooad/Model/Feedback.java
@@ -1,5 +1,56 @@
 package com.bookonrails.ooad.Model;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Feedback {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    private User user;
+    private Ticket train;
+    private int rating;
+    private String comment;
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Ticket getTrain() {
+        return train;
+    }
+
+    public void setTrain(Ticket train) {
+        this.train = train;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
 }
