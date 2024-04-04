ff57ad17b82c1f3ac91bfb4a53525ff28315435b
shreyamishra414@gmail.com <shreyamishra414@gmail.com>
2024-04-04 13:39:17 +0530

    Add In-Train Menu model

--- ooad/src/main/java/com/bookonrails/ooad/Model/InTrainMenu.java
+++ ooad/src/main/java/com/bookonrails/ooad/Model/InTrainMenu.java
@@ -1,5 +1,31 @@
 package com.bookonrails.ooad.Model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.List;
+
+@Entity
 public class InTrainMenu {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) 
+    private Long Id;
+    private List<MenuItem> items;
     
+    public Long getId() {
+        return Id;
+    }
+    public void setId(Long id) {
+        Id = id;
+    }
+    public List<MenuItem> getItems() {
+        return items;
+    }
+    public void setItems(List<MenuItem> items) {
+        this.items = items;
+    }
+    
+
 }
