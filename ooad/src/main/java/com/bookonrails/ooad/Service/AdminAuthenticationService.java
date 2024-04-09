5f76c66469e9ad1ecc92f699042e16d50afc5134
akankshamulgund <akankshamulgund@gmail.com>
2024-04-09 14:08:20 +0530

    Add admin controller, service, repository, and templates

---
+++ ooad/src/main/java/com/bookonrails/ooad/Service/AdminAuthenticationService.java
@@ -0,0 +1,8 @@
+// AdminAuthenticationService.java
+
+package com.bookonrails.ooad.Service;
+
+
+public interface AdminAuthenticationService {
+    boolean authenticate(String username, String password);
+}
