5f29bd6a30a07e95198f9a3bfd9f3d11c678af12
aditigoyal291 <goyaladiti2912@gmail.com>
2024-04-09 10:47:08 +0530

    Made some frontend pages

---
+++ ooad/src/main/java/com/bookonrails/ooad/Controller/Form.java
@@ -0,0 +1,21 @@
+package com.bookonrails.ooad.Controller;
+
+public class Form {
+    String name;
+    String email;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    
+    
+}
