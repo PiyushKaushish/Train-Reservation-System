9cdf5d7199d3f7dd5920098594049fb5270b5dd2
shreyamishra414@gmail.com <shreyamishra414@gmail.com>
2024-04-09 11:36:46 +0530

    Home page added and cookie added for login

--- ooad/src/main/java/com/bookonrails/ooad/FrontendController/UserFrontendContoller.java
+++ ooad/src/main/java/com/bookonrails/ooad/FrontendController/UserFrontendContoller.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.*;
 import com.bookonrails.ooad.Model.User;
 import com.bookonrails.ooad.Service.UserService;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+
 
 @Controller
 @RequestMapping(path="/users")
@@ -19,18 +22,31 @@ public class UserFrontendContoller {
     
     @GetMapping(path="/login")
     public String login(Model m){
-        m.addAttribute("message", "Please login");
+        m.addAttribute("message", "Login Here...");
         m.addAttribute("User", new User());
         return "login";
     }
 
-    @PostMapping(path="/submitForm")
-    public @ResponseBody User submitForm(@ModelAttribute("User") User user, Model model) {
+    @PostMapping(path="/loginForm")
+    public String loginForm(@ModelAttribute("User") User user, Model model,  HttpServletResponse response) {
         
         String username = user.getUsername();
         String password = user.getPassword();
         
         User u= userService.login(username, password);
-        return u; // Return user for now
+        if (u != null) {
+            // If login is successful, set a cookie to maintain the login state
+            Cookie cookie = new Cookie("username", username);
+            cookie.setMaxAge(7 * 24 * 60 * 60); // Cookie expires in 7 days
+            cookie.setPath("/"); // Set cookie path to root
+            response.addCookie(cookie);
+            
+            // Redirect the user to home page or any other page
+            return "redirect:/"; // 
+        } else {
+            // If login fails, return an error message or redirect back to the login page
+            String errorMessage = "Invalid username or password. Please try again.";
+            return "redirect:/users/login?error="+errorMessage;
+        }
     }
 }
