78ad3e7a1f7a951ee1c1053a7a1ed794a9b153e4
shreyamishra414@gmail.com <shreyamishra414@gmail.com>
2024-04-09 17:30:41 +0530

    Admin login and dashboard integrated with frontend and barrier applied with cookie

--- ooad/src/main/java/com/bookonrails/ooad/Service/AdminAuthenticationService.java
+++ ooad/src/main/java/com/bookonrails/ooad/Service/AdminAuthenticationService.java
@@ -1,8 +1,33 @@
-// AdminAuthenticationService.java
-
 package com.bookonrails.ooad.Service;
 
+import com.bookonrails.ooad.Model.Admin;
+import com.bookonrails.ooad.Repository.AdminRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-public interface AdminAuthenticationService {
-    boolean authenticate(String username, String password);
+@Service
+public class AdminAuthenticationService {
+
+    @Autowired
+    private AdminRepository adminRepository;
+
+    public Admin authenticate(Admin a) {
+        // Implement authentication logic by checking credentials in the database
+        Admin admin = adminRepository.findByUsernameAndPassword(a.getUsername(), a.getPassword());
+        return admin;
+    }
+
+    public void saveAdmin(Admin admin) {
+        adminRepository.save(admin);
+    }
+
+    public void deleteAdmin(Long id) {
+        adminRepository.deleteById(id);
+    }
+
+    public Admin getAdmin(Long id) {
+        return adminRepository.findById(id).get();
+    }
+
+
 }
