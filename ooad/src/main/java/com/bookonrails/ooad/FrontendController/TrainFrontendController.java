92365cd341dfe56d2d92930a54acac923d030245
shreyamishra414@gmail.com <shreyamishra414@gmail.com>
2024-04-10 21:03:03 +0530

    Operating Day rep and service added and dashboard updated

--- ooad/src/main/java/com/bookonrails/ooad/FrontendController/TrainFrontendController.java
+++ ooad/src/main/java/com/bookonrails/ooad/FrontendController/TrainFrontendController.java
@@ -8,7 +8,7 @@ import org.springframework.ui.Model;
 // import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import com.bookonrails.ooad.Model.ClassType;
+// import com.bookonrails.ooad.Model.ClassType;
 
 @Controller
 @RequestMapping(path="/train")
