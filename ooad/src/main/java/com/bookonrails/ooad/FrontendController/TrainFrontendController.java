374b9599d8b0b7c725b5b2ed1f089f1d46063b20
shreyamishra414@gmail.com <shreyamishra414@gmail.com>
2024-04-10 19:46:56 +0530

    integrating search train

---
+++ ooad/src/main/java/com/bookonrails/ooad/FrontendController/TrainFrontendController.java
@@ -0,0 +1,32 @@
+package com.bookonrails.ooad.FrontendController;
+
+
+
+// import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+// import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import com.bookonrails.ooad.Model.ClassType;
+
+@Controller
+@RequestMapping(path="/train")
+@CrossOrigin(origins = "*")
+public class TrainFrontendController {
+    @GetMapping(path="/search")
+    public String searchTrain() {
+        return "train/search_train";
+    }
+
+    @PostMapping(path="/search")
+    public String searchTrain(@RequestParam("date") String date, @RequestParam("fromStation") String source, @RequestParam("toStation") String destination,@RequestParam("classType") String classType, Model model) {
+        System.out.println(date+ " "+ source + " "+ destination+" "+ classType);
+        // fix input format and then send the trains
+        return "train/search_train";
+    }
+
+
+
+    
+}
