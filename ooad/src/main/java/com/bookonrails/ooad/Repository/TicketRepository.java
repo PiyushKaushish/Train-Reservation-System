a642b9924f5edfc9a6572f34fa49a8386251f255
shreyamishra414@gmail.com <shreyamishra414@gmail.com>
2024-04-15 22:29:41 +0530

    seat allocation methods defined

--- ooad/src/main/java/com/bookonrails/ooad/Repository/TicketRepository.java
+++ ooad/src/main/java/com/bookonrails/ooad/Repository/TicketRepository.java
@@ -6,12 +6,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.bookonrails.ooad.Model.Ticket;
+import com.bookonrails.ooad.Model.TicketStatus;
 import com.bookonrails.ooad.Model.User;
 
 
 @Repository
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
-    Ticket findByPNR(String PNR);
-    List<Ticket> findByUser(User user);
-    List<Ticket> findByTrain_TrainNo(String trainNo);
+    public Ticket findByPNR(String PNR);
+    public List<Ticket> findByUser(User user);
+    public List<Ticket> findByTrain_TrainNo(String trainNo);
+    public List<Ticket> findByStatus(TicketStatus status);
 }
